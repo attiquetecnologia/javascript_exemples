806d5000f41f1a060f24800859607c2475956134
Rodrigo Attique <rodrigoatique@gmail.com>
2026-09-01 21:18:05 -0300

    modelo javascript

---
+++ modelo.js
@@ -0,0 +1,8 @@
+ /** 
+* @descricao Arquivo responsável pelo processamento, validação e cálculo de médias de notas escolares. 
+* @nome_arquivo processador_notas.js 
+* @nome_exercicio Atividade 01 - Práticas de Clean Code 
+* @nome_aluno Seu Nome Completo 
+* @email_aluno <seu.email@exemplo.com> 
+* @turma Sua Turma (Ex: Engenharia de Software - 2026) 
+*/
